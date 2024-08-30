2ffec47216daaa7c718be69057136947047aca22
Haomin Kong <a645162@qq.com>
2024-08-30 18:28:55 +0800

    chore(Lib): Bump lib version

--- mock/FakeData/common.ts
+++ mock/FakeData/common.ts
@@ -1,21 +1,54 @@
+import { random, randomInt } from 'es-toolkit';
+
+export const ensureInt = (num: number): number => {
+  // 四舍五入
+  return Math.round(num);
+};
+
 // 获取随机整数
-export const getRandomInt = (max: number, min: number = 0) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+// export const getRandomInt = (max: number, min: number = 0) => {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// };
+export const getRandomInt = (maxValue: number, minValue: number = 0) => {
+  let min = ensureInt(minValue);
+  let max = ensureInt(maxValue);
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  if (min === max) {
+    return min;
+  }
+
+  return randomInt(min, max);
 };
 
 // 获取随机小数
 export const getRandomFloat = (
-  max: number,
-  min: number = 0,
+  maxValue: number,
+  minValue: number = 0,
   fixed: number = 2,
 ): number => {
-  let value = Math.random() * (max - min) + min;
+  let min = minValue;
+  let max = maxValue;
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  if (min === max) {
+    return min;
+  }
+
+  //   let value = Math.random() * (max - min) + min;
+  let value = random(min, max);
 
   return Number(value.toFixed(fixed));
 };
 
 export const getRandomBool = (trueP: number = 50) => {
-  return getRandomInt(100, 0) < trueP;
+  return getRandomInt(100) < trueP;
 };
 
 export const ceilAndTruncate = (num: number): string => {
