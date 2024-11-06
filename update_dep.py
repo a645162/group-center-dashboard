b61ce06bb84d05d169dbf666aab49615e865d158
Haomin Kong <a645162@qq.com>
2024-11-06 16:12:51 +0800

    chore(Lib): Bump lib version

--- update_dep.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system("npx ncu -u")
-
-os.system("pnpm i")
-os.system("bun i")
--- upgrade.py
+++ upgrade.py
@@ -1,10 +1,14 @@
 import os
 
-os.system("ncu -u")
+os.system("bun run ncu -u")
 os.system("bun i")
+
+ret=os.system("bun run build")
+if ret != 0:
+    exit(ret)
+
 os.system("pnpm i")
 os.system("bun i")
-os.system("bun format")
-os.system("bun build")
+os.system("bun run format")
 
 # chore(Lib): Bump lib version
