fa80ea6c670440422f71aee96abf81ec667ba3d0
Haomin Kong <a645162@qq.com>
2024-12-22 22:15:05 +0800

    chore(Lib): Bump lib version

--- upgrade.py
+++ upgrade.py
@@ -1,13 +1,30 @@
 import os
+import shutil
 
+
+def remove_node_modules():
+    node_module_path = os.path.join(os.getcwd(), "node_modules")
+    if os.path.exists(node_module_path):
+        # Remove
+        shutil.rmtree(node_module_path)
+
+
+# Install dependencies
+os.system("bun i")
+# Upgrade dependencies
 os.system("bun run ncu -u")
+# Reinstall dependencies
 os.system("bun i")
 
-ret=os.system("bun run build")
+# Build Test
+ret = os.system("bun run build")
 if ret != 0:
     exit(ret)
 
+# Remove node_modules before running pnpm install
+remove_node_modules()
 os.system("pnpm i")
+
 os.system("bun i")
 os.system("bun run format")
 
